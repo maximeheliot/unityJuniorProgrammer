8e2ba2edd51e60621c2bd312ff1f63b514735d47
maximeheliot <maxime.heliotpro@gmail.com>
2021-01-17 20:35:17 +0100

    Saving Persistent Player Data

---
+++ Saving Persistent Player Data/Assets/_Demo/Scripts/CharacterSaveData_SO.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character Save Data", menuName = "Character/Data", order = 1)]
+public class CharacterSaveData_SO : ScriptableObject
+{
+    [Header("Stats")]
+
+    [SerializeField]
+    int currentHealth;
+
+    [Header("Leveling")]
+
+    [SerializeField]
+    int currentLevel = 1;
+
+    [SerializeField] private int maxLevel = 30;
+    [SerializeField] private int basisPoints = 200;
+    [SerializeField] private int pointsTillNextLevel;
+
+    [SerializeField]
+    float levelBuff = 0.1f;
+
+    [Header("Save Data")] 
+    
+    [SerializeField] private string key;
+    
+
+    public float LevelMultiplier
+    {
+        get { return 1 + (currentLevel - 1) * levelBuff; }
+    }
+
+    public int CurrentHealth
+    {
+        get { return currentHealth; }
+        set { currentHealth = value; }
+    }
+
+    public void AggregateAttackPoints(int points)
+    {
+        pointsTillNextLevel -= points;
+
+        if (pointsTillNextLevel <= 0)
+        {
+            currentLevel = Mathf.Clamp(currentLevel + 1, 0, maxLevel);
+
+            pointsTillNextLevel += (int) (basisPoints * LevelMultiplier);
+            
+            Debug.Log("LEVEL UP! New Level: " + currentLevel);
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (pointsTillNextLevel == 0)
+        {
+            pointsTillNextLevel += (int) (basisPoints * LevelMultiplier);
+        }
+        
+        JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(key), this);
+    }
+
+    private void OnDisable()
+    {
+        if (key == "")
+        {
+            key = name;
+        }
+
+        string jsonData = JsonUtility.ToJson(this, true);
+        PlayerPrefs.SetString(key, jsonData);
+        PlayerPrefs.Save();
+    }
+}
