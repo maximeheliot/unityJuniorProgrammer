9df58b6e959b77dba77fe9b2e5e458e1c9e6ed5f
maximeheliot <maxime.heliotpro@gmail.com>
2021-01-18 21:27:19 +0100

    Start game manager

---
+++ Game Managers/Assets/Game Manager/Scripts/GameManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    public GameObject[] systemPrefabs;
+
+    private List<GameObject> _instancedSystemPrefabs;
+    private string _currentLevelName = string.Empty;
+    private List<AsyncOperation> _loadOperations;
+
+    // What level the game is currently in
+    
+    // Keep track of the game state
+    
+    // Generate other persistent systems
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        _instancedSystemPrefabs = new List<GameObject>();
+        _loadOperations = new List<AsyncOperation>();
+
+        InstanciateSystemPrefabs();
+        
+        LoadLevel("Main");
+    }
+
+    #region Load and Unload game levels
+
+    void OnLoadOperationComplete(AsyncOperation ao)
+    {
+        if (_loadOperations.Contains(ao))
+        {
+            _loadOperations.Remove(ao);
+            
+            // dispatch message
+            // transition between scenes
+        }
+        
+        Debug.Log("Load Complete.");
+    }
+
+    void OnUnloadOperationComplete(AsyncOperation ao)
+    {
+        Debug.Log("Unload Complete.");
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+        if (ao == null)
+        {
+            Debug.Log("[GameManager] Unable to load level " + levelName);
+            return;
+        }
+        
+        ao.completed += OnLoadOperationComplete;
+        _loadOperations.Add(ao);
+        
+        _currentLevelName = levelName;
+    }
+
+    public void UnloadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+        if (ao == null)
+        {
+            Debug.Log("[GameManager] Unable to unload level " + levelName);
+            return;
+        }
+        
+        ao.completed += OnUnloadOperationComplete;
+    }
+
+    #endregion
+
+    void InstanciateSystemPrefabs()
+    {
+        foreach (var obj in systemPrefabs)
+        {
+            var prefabInstance = Instantiate(obj);
+            _instancedSystemPrefabs.Add(prefabInstance);
+        }
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        foreach (var obj in _instancedSystemPrefabs)
+        {
+            Destroy(obj);
+        }
+        _instancedSystemPrefabs.Clear();
+    }
+}
---
+++ Game Managers/Assets/Game Manager/Scripts/Utils/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T instance { get; private set; }
+    public static bool isInitialized => instance != null;
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("[Singleton] Trying to instantiate a second instance of a singleton class." );
+        }
+        else
+        {
+            instance = (T) this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+}
